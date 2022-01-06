06c025311a3f65c070d5db83d337f2fad5709fc4
Negitoro001 <stnjllx7157@mc.pikara.ne.jp>
2022-01-06 19:45:07 +0900

    共通部分が完成した

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,43 +1,40 @@
 <template>
-  <div>
+  <div
+    id="app"
+    class="container"
+  >
+    <section>
+      <b-sidebar
+        type="is-light"
+        :overlay="overlay"
+        :right="right"
+        v-model="open"
+        :fullheight="fullheight"
+      >
+        <b-menu>
+          <b-menu-list>
+            <b-menu-item
+              icon="arrow-left-top-bold"
+              label="トップページ"
+            ></b-menu-item>
+            <b-menu-item
+              icon="text-account"
+              label="略歴"
+            ></b-menu-item>
+            <b-menu-item
+              icon="information-outline"
+              label="ブログ"
+            ></b-menu-item>
+            <b-menu-item
+              icon="form-select"
+              label="問い合わせフォーム"
+            ></b-menu-item>
+          </b-menu-list>
+        </b-menu>
+      </b-sidebar>
 
-    <section class="main-content columns">
-      <aside class="column is-2 section">
-        <b-sidebar
-          type="is-success"
-          :overlay="overlay"
-          :right="right"
-          v-model="open"
-          position="fixed"
-          :fullheight="fullheight"
-        >
-          <b-menu>
-            <b-menu-list>
-              <b-menu-item
-                icon="arrow-left-top-bold"
-                label="トップページ"
-              />
-              <b-menu-item
-                icon="text-account"
-                label="略歴"
-              />
-              <b-menu-item
-                icon="information-outline"
-                label="ブログ"
-              />
-              <b-menu-item
-                icon="form-select"
-                label="問い合わせフォーム"
-              />
-            </b-menu-list>
-          </b-menu>
-        </b-sidebar>
-      </aside>
-
-      <div class="container column is-10">
-        <Nuxt />
-      </div>
     </section>
+    <Nuxt />
   </div>
 </template>
 
@@ -46,6 +43,7 @@ export default {
   data() {
     return {
       fullheight: true,
+      fullwidth: true,
       overlay: false,
       right: false,
       open: true,
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="main-title">
-    Abstract Artist
-  </div>
+  <section>
+    <div class="main-title">
+      Abstract<br>
+      Artist
+    </div>
+    <div class="name-class">
+      Taichi Nagayama
+      in Japan
+    </div>
+  </section>
 </template>
