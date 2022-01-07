9f23d8bcc12cf5af80b145730b89a5a2d1177a98
Negitoro001 <stnjllx7157@mc.pikara.ne.jp>
2022-01-07 13:03:51 +0900

    文章を追加(暫定メインページ完成)

--- pages/index.vue
+++ pages/index.vue
@@ -8,5 +8,24 @@
       Taichi Nagayama,
       in Japan
     </div>
+
+    <br>
+
+    <div class="main-sentense-japanese">
+      私は、日本で抽象芸術家をしています「長山太一」といいます。<br>
+      私は自然が大好きで、形を持たない(無形の)世界を描くことに幸せを感じます。<br>
+      この文章を見てくださった皆様に、私の絵の心境が伝わりますように、と願っています。<br>
+      私は必ず、世界で活躍する芸術家になれると信じています。
+    </div>
+
+    <br>
+    <br>
+
+    <div class="main-sentense-english">
+      I am Nagayama Taichi, a Japanese abstract artist. <br>
+      I love natural environment, and It's makes me so happy that to draw the intangible image. <br>
+      I wish I tell you to express "my will of my arts". <br>
+      I believe I'm gonna be the artist to active all over the world.
+    </div>
   </section>
 </template>
